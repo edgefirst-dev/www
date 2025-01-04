26cce200e046272f5c342ba9c9fab47a72e76bba
Sergio Xalambrí <hello@sergiodxa.com>
2025-01-04 01:48:21 -0500

    Setup and add landing

--- app/views/landings/home.tsx
+++ app/views/landings/home.tsx
@@ -1,24 +1,68 @@
-import { AnchorButton } from "app:components/anchor-button";
+import { ArrowRight, Copy, CopyCheck } from "lucide-react";
+import { useEffect, useState } from "react";
+import { Button } from "react-aria-components";
 import type { Route } from "./+types/home";
 
 export default function Home(_: Route.ComponentProps) {
 	return (
-		<div className="flex flex-col justify-center items-center gap-y-6 max-w-lg">
-			<h1 className="text-4xl/none font-semibold text-center text-balance">
-				Edge-first Starter Kit for React
+		<main className="bg-white dark:bg-black text-black dark:text-white min-h-dvh w-screen flex flex-col items-center justify-center gap-6 text-center px-4">
+			<h1 className="text-pretty font-bold text-3xl sm:text-5xl md:text-6xl lg:text-7xl dark:text-neutral-50">
+				Edge-first
+				<br />
+				Starter Kit for React
 			</h1>
 
-			<p className="text-lg/normal text-center text-balance">
-				Build lightning-fast React applications with edge computing. Deploy
-				globally and scale effortlessly.
+			<p className="mx-auto max-w-3xl text-pretty text-lg text-neutral-800 sm:font-light sm:text-base md:text-xl dark:text-neutral-200">
+				Ship to the Edge with the{" "}
+				<span className="text-accent-500">One Person Starter Kit</span>. DB, KV,
+				FS, AI & Auth. All Open Source.
 			</p>
 
-			<AnchorButton
-				reloadDocument
-				to="https://github.com/edgefirst-dev/starter"
-			>
-				Get Started
-			</AnchorButton>
-		</div>
+			<Command />
+		</main>
+	);
+}
+
+const command = "bun create edgefirst-dev/starter";
+
+function Command() {
+	let [copied, setCopied] = useState(false);
+
+	useEffect(() => {
+		if (copied) {
+			let timeout = setTimeout(setCopied, 5000, false);
+			return () => clearTimeout(timeout);
+		}
+	}, [copied]);
+
+	return (
+		<Button
+			type="button"
+			className="items-center gap-2 rounded-md bg-neutral-100 px-3 py-1.5 font-light font-mono text-neutral-950 text-xs/none shadow focus:outline-2 focus:outline-accent-400 focus:outline-offset-2 flex dark:bg-neutral-900 dark:text-neutral-50"
+			onPress={() => {
+				navigator.clipboard.writeText(command).then(() => setCopied(true));
+			}}
+		>
+			<span>{command}</span>
+			{copied ? (
+				<CopyCheck aria-hidden className="size-4" />
+			) : (
+				<Copy aria-hidden className="size-4" />
+			)}
+		</Button>
+	);
+}
+
+function GetStarted() {
+	return (
+		<a
+			className="button relative rounded-md bg-accent-500 px-5 py-2.5 font-light text-accent-50 text-base/none shadow-md focus:outline-2 focus:outline-accent-400 focus:outline-offset-2 dark:bg-accent-500 dark:text-accent-950 dark:shadow-none"
+			href="https://github.com/edgefirst-dev/stack"
+		>
+			<span className="flex relative gap-2">
+				<span>Get Started</span>
+				<ArrowRight aria-hidden className="size-5" />
+			</span>
+		</a>
 	);
 }
